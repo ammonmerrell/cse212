931ce913e2a199ffcb22c03c8235f2123bac1aca
ammonmerrell <ammonmerrell@outlook.com>
2026-01-17 11:27:07 -0700

    created first test

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using Microsoft.VisualBasic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 // TODO Problem 2 - Write and run test cases and fix the code to match requirements.
@@ -6,13 +8,22 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: two object are added, both with 1 priority. When one is dequed, the first one will leave the list.
+    // Expected Result: [b (pri:1)]
+    // Defect(s) Found: none
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("a", 1);
+        priorityQueue.Enqueue("b", 1);
+        Debug.WriteLine(priorityQueue);
+        var a = priorityQueue.Dequeue();
+        Debug.WriteLine(a);
+        Debug.WriteLine(priorityQueue);
+        if(a != "a"){
+            Assert.Fail("Implement the test case and then remove this.");
+        }
     }
 
     [TestMethod]
